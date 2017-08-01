ea93979d36b86a391d6510b789549ff007a76047
Matthew Tillett <matthew@beloud.co.uk>
2017-08-01 22:56:55 +0100

    Added 'Print At' sub-routine.

--- includes/library.asm
+++ includes/library.asm
@@ -1,6 +1,5 @@
 
 
-;
 ; Clear entire screen
 ; Version       : v1.0
 ; Created       : 01/01/1971
@@ -22,9 +21,57 @@ fn_ClearScreen:
                         pop     bc
                         pop     hl
                         ret
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+; Prints a string at a specified screen location
+; Version       : v1.0
+; Created       : 01/08/2017
+; Author        : Matthew Tillett
 ;
+; Data Structure: ix+0 : Length of string
+;               : ix+1 : AT line (Y-position)
+;               : ix+2 : AY column (X-position)
+;               : ix+3 : String
+;
+; Example
+;
+;                 ld      hl, _strScore         ; Label address of string structure
+;                 call    fn_PrintAt            ; Call subroutine
+; _strScore:      db      5,0,20,"Score"        ; Label with strings structure
+
+fn_PrintAt:
+                        push    hl                      ; Push onto stack label address
+                                                        ; Calls to $1601 modifies hl
+
+                        ld      a, 2                    ; Output to upper screen (0-21)
+                        call    $1601                   ; Open channel
+
+                        ld      a, 22                   ; Print at
+                        rst     16                      ;
+
+                        pop    hl                       ; Restore label address
+
+                        ld      b, (hl)                 ; Length of string to output
+
+                        inc     hl                      ; Grab line number to print at (y-pos)
+                        ld      a, (hl)
+                        rst     16
+
+                        inc     hl                      ; Grab column number to print at (x-pos)
+                        ld      a, (hl)
+                        rst     16
+
+_printLine:
+                        inc     hl                      ; Loop through characters
+                        ld      a, (hl)                 ;
+                        rst     16                      ; ..and display
+                        djnz    _printLine              ; Decrease 'b'. If b != 0, do jump to label
+
+                        ret
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 ; Test function
 ; Version       : v1.0
 ; Created       : 29/07/2014
@@ -37,3 +84,5 @@ fn_Test:
                         ld      a, #1
                         ld      a, #1
                         ret
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
